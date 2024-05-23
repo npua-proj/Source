bd5a2371d7f8063f2944c42223e655d3cd31edbc
Andranik Grigoryna <andranik@grigoryan.work>
2024-05-23 12:56:16 +0300

    Ci setup preparation

--- config/config.py
+++
@@ -1,8 +0,0 @@
-# config/config.py
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
-REMOTEOK_API_URL = 'https://remoteok.io/api'
--- job_sources/__init__.py
+++
@@ -1,8 +0,0 @@
-# job_sources/__init__.py
-from job_sources.remoteok import RemoteOK
-
-def fetch_all_jobs():
-    jobs = []
-    remoteok = RemoteOK()
-    jobs.extend(remoteok.fetch_jobs())
-    return jobs
